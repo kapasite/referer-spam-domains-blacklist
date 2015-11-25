ae24b0043a675bcf01cc101c2b8b30f8d3248a9a
desbma <desbma@users.noreply.github.com>
2015-11-25 22:02:24 +0100

    Dead domain script: Cross check with 5 DNS servers

--- remove-dead-domains.py
+++ remove-dead-domains.py
@@ -8,20 +8,26 @@ import subprocess
 import threading
 
 
-domains_done_count = 0
+DNS_SERVERS = ("8.8.8.8",  # Google DNS
+               "208.67.222.222",  # OpenDNS
+               "84.200.69.80",  # DNS.WATCH
+               "209.244.0.3",  # Level3 DNS
+               "8.26.56.26")  # Comodo Secure DNS
+
+checks_done_count = 0
 domains_count = 0
 domains_count_lock = threading.Lock()
 
 
-def is_domain_dead(domain):
-  """ Return True if domain has a DNA A record, False instead. """
-  cmd = ("dig", "+short", "+time=10", "+tries=6", domain)
+def is_domain_dead(domain, dns_server):
+  """ Return True if domain has a DNA A record on this DNS server, False instead. """
+  cmd = ("dig", "+short", "+time=10", "+tries=6", "@%s" % (dns_server), domain)
   output = subprocess.check_output(cmd)
   # update progress
-  global domains_done_count, domains_count, domains_count_lock
+  global checks_done_count, domains_count, domains_count_lock
   with domains_count_lock:
-    domains_done_count += 1
-    print("%u/%u domains checked" % (domains_done_count, domains_count), end="\r")
+    checks_done_count += 1
+    print("%u/%u domains checked" % (checks_done_count // len(DNS_SERVERS), domains_count), end="\r")
   return not bool(output)
 
 
@@ -43,16 +49,20 @@ if __name__ == "__main__":
         domains.append(domain)
         with domains_count_lock:
           domains_count += 1
-        future = executor.submit(is_domain_dead, domain)
-        futures.append(future)
+        domain_futures = []
+        for dns_server in DNS_SERVERS:
+          future = executor.submit(is_domain_dead, domain, dns_server)
+          domain_futures.append(future)
+        futures.append(domain_futures)
 
   # write new file
   domains_removed_count = 0
   with open(args.list_file, "wt") as list_file:
-    for future, domain in zip(futures, domains):
-      dead = future.result()
-      if not dead:
-        list_file.write("%s\n" % (domain))
-      else:
+    for domain_futures, domain in zip(futures, domains):
+      domain_futures_results = tuple(f.result() for f in domain_futures)
+      # consider domain is dead only if all DNS servers have no entry
+      if all(domain_futures_results):
         domains_removed_count += 1
+      else:
+        list_file.write("%s\n" % (domain))
   print("\n%u dead domains removed" % (domains_removed_count))
